b91801d69779c05731c669d398da416731aad16e
Giulio <giuliopanza2000@gmail.com>
2025-02-11 15:07:56 +0100

    finish

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 /*Esercizio
-Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità
-sulla base di quello che impareremo.
-Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
-Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo,
-contenuto, immagine e tags (tags è un array di stringhe)
-Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post.
-Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare
-le immagini associate ad ogni post.
-Testare su postman
-Buon lavoro!*/
+Esercizio
+Creare un file di routing (routers/posts.js) che conterrà le rotte necessario per l'entità post.
+All'interno creare le rotte per le operazioni CRUD (Index, Show, Create, Update e Delete)
+Tutte le risposte saranno dei testi che confermeranno l’operazione che il server deve eseguire, 
+secondo le convenzioni REST.
+Ad esempio:
+Se viene chiamata /posts col verbo GET ci aspettiamo “Lista dei post”;
+Se viene chiamato /posts/1 col verbo DELETE ci aspettiamo “Cancellazione del post 1”
+e via dicendo…
+Registrare il router dentro app.js con il prefisso posts/.*/
 
 const express = require("express");
 
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
     )
 });
 
-app.use("/api/post", postsRouter)
+app.use("/api/posts", postsRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
